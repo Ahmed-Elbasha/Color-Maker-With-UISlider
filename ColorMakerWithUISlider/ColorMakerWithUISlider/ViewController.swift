2cca4c12313c517be73bef6214762cc1b40ec367
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-24 13:41:13 +0200

    Finished the UI Design of View Controller

---
+++ ColorMakerWithUISlider/ColorMakerWithUISlider/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ColorMakerWithUISlider
+//
+//  Created by Ahmed Elbasha on 6/24/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
